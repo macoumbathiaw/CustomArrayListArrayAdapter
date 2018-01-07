5f5fb44f68206ee4aea8ee7b6562d22205923c01
Macoumba Thiaw <macoumbathiaw00@gmail.com>
2018-01-07 11:08:30 -0800

    Initial commit

---
+++ app/src/main/java/CustomWords.java
@@ -0,0 +1,28 @@
+/**
+ * Created by mthiaw on 1/7/18.
+ */
+
+public class CustomWords {
+
+    //Declare the variables
+    String mWolof;
+    String mEnglish;
+    int mImage;
+
+    //Create a default constructor without any variable
+    public CustomWords() {
+    }
+
+    //Create a default constructor with 2 variables
+    public CustomWords(String mWolof, String mEnglish) {
+        this.mWolof = mWolof;
+        this.mEnglish = mEnglish;
+    }
+
+    //Create a default constructor with 3 variables
+    public CustomWords(String mWolof, String mEnglish, int mImage) {
+        this.mWolof = mWolof;
+        this.mEnglish = mEnglish;
+        this.mImage = mImage;
+    }
+}
